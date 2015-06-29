43b1fa7a30c27c098468d3edcf1da4608186103d
Alan Heywood <alan@softweb.net.au>
2015-06-29 13:58:31 +1000

    passes the json params via file for better security

--- lib/docxtemplater.rb
+++ lib/docxtemplater.rb
@@ -3,13 +3,16 @@ require "json"
 
 module Docxtemplater
   def self.render(template_path, output_path, data)
-    command = generate_bash_command(template_path, output_path, data)
-    system command
-  end
+    data_tmp_file = Tempfile.new('docxtemplater').tap do |file|
+      file.write(data.to_json)
+      file.close
+    end
 
-  def self.generate_bash_command(template_path, output_path, data)
-    script_path = File.join(__dir__, 'call_docxtemplater.js')
-
-    "node #{script_path} #{template_path} #{output_path} #{Shellwords.escape(data.to_json)}"
+    begin
+      script_path = File.join(__dir__, 'call_docxtemplater.js')
+      system  "node #{script_path} #{template_path} #{output_path} #{data_tmp_file.path}"
+    ensure
+      data_tmp_file.unlink
+    end
   end
 end
--- spec/docxtemplater_spec.rb
+++ spec/docxtemplater_spec.rb
@@ -5,15 +5,6 @@ describe Docxtemplater do
     expect(Docxtemplater::VERSION).not_to be nil
   end
 
-  it 'generates the bash command' do
-    template_path = 'template.docx'
-    output_path = 'rendered.docx'
-    data = { animal: 'dog' }
-    command = Docxtemplater.generate_bash_command(template_path, output_path, data)
-    script_path = File.join(__dir__.gsub('/spec',''), 'lib', 'call_docxtemplater.js')
-    expect(command).to eq('node ' + script_path + ' template.docx rendered.docx ' + "\\{\\\"animal\\\":\\\"dog\\\"\\}")
-  end
-
   it 'renders the file' do
     template_path = File.join(__dir__, 'fixtures', 'template.docx')
     output_path = File.join(__dir__, 'rendered.docx')
@@ -21,5 +12,7 @@ describe Docxtemplater do
     data = { animal: 'dog' }
     Docxtemplater.render(template_path, output_path, data)
     expect(File).to exist(output_path)
+
+    puts "CHECK: manually check that the data was merged into #{output_path}"
   end
 end
